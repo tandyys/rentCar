665c2e55732224b24191f1be9d8214a50f9c6aa6
Tandy Sabana <tansabana12@gmail.com>
2024-07-17 20:38:28 +0700

    Initail commit
    Initail commit

---
+++ RentCar/RentCar/RentCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentCarApp.swift
+//  RentCar
+//
+//  Created by tandyys on 17/07/24.
+//
+
+import SwiftUI
+
+@main
+struct RentCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
